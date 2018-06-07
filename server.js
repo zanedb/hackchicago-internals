7e4c85e42433e9e37c05893d447d0ecd441fec46
Zane <contact@zane.sh>
2018-06-07 11:34:20 -0700

    Remove old API (using Firebase now)

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Attendee = require('./app/models/attendee');
 const ip = require("ip");
 const opn = require('opn');
 const path = require('path');
@@ -13,18 +12,10 @@ let server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
+// define port to run server on
 let port = process.env.PORT || 3000;
 
-// setup express router
-const router = express.Router();
-
-// handles all requests
-router.use(function(req, res, next) {
-  console.log('Request is being handled..');
-  next(); // continue to next route
-});
-
-// route requests to html files
+// route requests to HTML files
 let publicdir = __dirname + '/public';
 server.use(function(req, res, next) {
   if (req.path.indexOf('.') === -1) {
@@ -39,40 +30,6 @@ server.use(function(req, res, next) {
     next();
 });
 server.use(express.static(publicdir));
-
-// api/attendees
-router.route('/attendees')
-  // create an attendee (accessed at POST http://localhost:3000/api/attendees)
-  .post(function(req, res) {
-    let attendee = new Attendee();
-    // set attributes from request
-    attendee.fname = req.body.fname;
-    attendee.lname = req.body.lname;
-    attendee.email = req.body.email;
-    attendee.hex = ("hackchicago2018" + "/" + attendee.fname + "/" + attendee.lname + "/" + attendee.email).toUpperCase();
-
-    var hex, i;
-    var result = "";
-    for (i=0; i<this.length; i++) {
-      hex = this.charCodeAt(i).toString(16);
-      result += ("000"+hex).slice(-4);
-    }
-
-    res.json({
-      message: 'Attendee QR code created!',
-      hex: attendee.hex
-    });
-  })
-  // get all the attendees (accessed at GET http://localhost:3000/api/attendees)
-  .get(function(req, res) {
-    // TODO make this load attendees
-    res.status(404).json({ error: 'No attendees found' });
-  });
-
-// setup files to be served
-//server.use(express.static('public'));
-// prefix routes with /api
-server.use('/api', router);
 
 // start server
 server.listen(port);
